f73938170bea0234eaabdf4863e16071e41a4956
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-12-16 17:50:17 +0300

    обработка ошибок

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Server } from 'http';
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/user.controller";
+import { ExeptionFilter } from './errors/exeption.filter';
 
 export class App {
 
@@ -10,24 +11,32 @@ export class App {
     port: number;
     logger: LoggerService;
     userController: UserController;
+    exeptionFilter: ExeptionFilter;
 
 
     constructor(
             logger: LoggerService,
-            userController: UserController
+            userController: UserController,
+            exeptionFilter: ExeptionFilter
         ) {
             this.app = express();
             this.port = 8000;
             this.logger = logger;
             this.userController = userController;
+            this.exeptionFilter = exeptionFilter;
     }
     
     useRoutes() {
         this.app.use('/users', this.userController.router);
     }
 
+    useExeptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
     public async init() {
         this.useRoutes();
+        this.useExeptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
     }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response, Request } from "express";
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from '../errors/http-error.class';
 
 export class UserController extends BaseController {
     constructor(
@@ -14,7 +15,7 @@ export class UserController extends BaseController {
     }
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login')
+        next(new HTTPError(401, 'ошибка авторизации', 'login'));
     }
 
     register(req: Request, res: Response, next: NextFunction) {
